809c23529c49e628ef2d69ff8693b235eef8fce5
Rifmir Gatin <rifmir349@yandex.ru>
2021-09-12 20:48:27 +0300

    Рефакторинг кода, исправил условия

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
 fun main() {
     val likes = 61
 
-    if (likes % 2 == 1 || likes == 1) {
+    if (likes % 10 == 1 || likes == 1) {
         println("Нравится $likes человеку")
-    } else if (likes % 3 == 1) {
+    } else if (likes % 11 == 1) {
         println("Нравится $likes людям")
     } else {
         println("Нравится $likes людям")
